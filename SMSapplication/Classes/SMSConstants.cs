c07b054f41ae4e8f67860dd9c26bdff3fd82d5f4
cuong1503 <cuong.dh8c@gmail.com>
2016-02-09 17:15:27 +0700

    ok

---
+++ SMSapplication/Classes/SMSConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSapplication.Classes
+{
+    public static class SMSConstants
+    {
+        public static string ViettelProvider { get { return "Viettel"; } }
+        public static string VinaphoneProvider { get { return "Vinaphone"; } }
+        public static string MobiphoneProvider { get { return "Mobiphone"; } }
+
+        public static string PortComNameAvaiable_1 { get { return "PC UI Interface"; } }
+    }
+}
---
+++ SMSapplication/Classes/SettingDevice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSapplication.Classes
+{
+    public class SettingDevice
+    {
+        public string DeviceName { get; set; }
+
+        public string Cops { get; set; }
+
+        public int DelayTime { get; set; }
+
+        public int Limit { get; set; }
+        public SettingDevice(string name, string cops, int delay, int limit)
+        {
+            this.DeviceName = name;
+            this.Cops = cops;
+            this.DelayTime = delay;
+            this.Limit = limit;
+        }
+        public SettingDevice()
+        {
+          
+        }
+    }
+}
