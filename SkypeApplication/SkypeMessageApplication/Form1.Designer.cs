0d326653f21e6df75cf0d582e51b79170edfec6c
cuong1503 <cuong.dh8c@gmail.com>
2016-02-10 11:08:18 +0700

    lan dau tien

---
+++ SkypeApplication/SkypeMessageApplication/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SkypeMessageApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKetNoiSkype = new System.Windows.Forms.Button();
+            this.btnGuiTin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.colUser = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnKetNoiSkype
+            // 
+            this.btnKetNoiSkype.Location = new System.Drawing.Point(232, 343);
+            this.btnKetNoiSkype.Name = "btnKetNoiSkype";
+            this.btnKetNoiSkype.Size = new System.Drawing.Size(275, 44);
+            this.btnKetNoiSkype.TabIndex = 0;
+            this.btnKetNoiSkype.Text = "Kết nối skype";
+            this.btnKetNoiSkype.UseVisualStyleBackColor = true;
+            this.btnKetNoiSkype.Click += new System.EventHandler(this.btnKetNoiSkype_Click);
+            // 
+            // btnGuiTin
+            // 
+            this.btnGuiTin.Location = new System.Drawing.Point(232, 314);
+            this.btnGuiTin.Name = "btnGuiTin";
+            this.btnGuiTin.Size = new System.Drawing.Size(75, 23);
+            this.btnGuiTin.TabIndex = 1;
+            this.btnGuiTin.Text = "Gửi tin";
+            this.btnGuiTin.UseVisualStyleBackColor = true;
+            this.btnGuiTin.Click += new System.EventHandler(this.btnGuiTin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colUser});
+            this.listView1.Location = new System.Drawing.Point(3, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(219, 383);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // colUser
+            // 
+            this.colUser.Text = "User";
+            this.colUser.Width = 138;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(229, 2);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(44, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Refresh";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 122);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 186);
+            this.textBox1.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(233, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(230, 27);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(44, 13);
+            this.linkLabel2.TabIndex = 4;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Refresh";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(234, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 392);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuiTin);
+            this.Controls.Add(this.btnKetNoiSkype);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKetNoiSkype;
+        private System.Windows.Forms.Button btnGuiTin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader colUser;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ SkypeApplication/SkypeMessageApplication/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SKYPE4COMLib;
+
+namespace SkypeMessageApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Skype skypeInstant;
+        private void btnKetNoiSkype_Click(object sender, EventArgs e)
+        {
+            skypeInstant = new Skype();
+            if (!skypeInstant.Client.IsRunning)
+            {
+                // start minimized with no splash screen
+                skypeInstant.Client.Start(true, true);
+            }
+            skypeInstant.Attach(6, true);
+        }
+
+        private void btnGuiTin_Click(object sender, EventArgs e)
+        {
+            string[] users = new string[] { "endyquach", "dongnv92" };
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                skypeInstant.SendMessage(users[i], "bot sent");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ThreadStart(loadContacts));
+            thread.IsBackground = true;
+            thread.Priority = System.Threading.ThreadPriority.AboveNormal;
+            thread.Name = "Load Skype Contacts";
+            thread.Start();
+        }
+
+        List<string> alleKontakte = new List<string>();
+        public void loadContacts() // Radiobutton2 = online | radiobutton1 = alle
+        {
+            alleKontakte.Clear();
+            if (radioButton1.Checked)
+            {
+                foreach (User user in skypeInstant.Friends)
+                {
+                    alleKontakte.Add(user.Handle);
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                foreach (User user in skypeInstant.Friends)
+                {
+                    if (user.OnlineStatus == TOnlineStatus.olsOnline | user.OnlineStatus == TOnlineStatus.olsNotAvailable | user.OnlineStatus == TOnlineStatus.olsDoNotDisturb | user.OnlineStatus == TOnlineStatus.olsAway)
+                    {
+                        alleKontakte.Add(user.Handle);
+                    }
+                }
+            }
+
+            MethodInvoker lvUpdate = delegate
+            {
+                listView1.Items.Clear();
+
+                foreach (var user in alleKontakte)
+                {
+                    listView1.Items.Add(user);
+                }
+                listView1.Sorting = SortOrder.Ascending;
+                if (radioButton2.Checked)
+                    radioButton2.Text = String.Format("nur online Kontakte ({0})", listView1.Items.Count);
+                else if (radioButton1.Checked)
+                    radioButton1.Text = String.Format("alle Kontakte ({0})", listView1.Items.Count);
+            };
+
+            Invoke(lvUpdate);
+        }
+    }
+}
